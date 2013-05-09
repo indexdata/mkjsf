39aab9ad9182a2ce681e64abae2011471b04a2d7
Niels Erik G. Nielsen <nielserik@indexdata.com>
2013-05-09 09:48:31 -0400

    Adds init response data object

---
+++ src/main/java/com/indexdata/mkjsf/pazpar2/data/InitResponse.java
@@ -0,0 +1,11 @@
+package com.indexdata.mkjsf.pazpar2.data;
+
+public class InitResponse extends Pazpar2ResponseData {
+
+  private static final long serialVersionUID = -1479775157276901600L;
+
+  public InitResponse() {
+    // TODO Auto-generated constructor stub
+  }
+
+}
